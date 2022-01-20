51497b476a9e9b5bfe06d07541f2b4fd4a5cc2d9
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-01-20 14:29:30 +0000

    Update home page content

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,48 @@
 const home = function() {
+    const openingTimesArr = [
+        {
+            'days': 'Monday - Thursday',
+            'times': '11am - 9pm'
+        },
+        {
+            'days': 'Friday & Saturday',
+            'times': '11am - 11pm'
+        },
+        {
+            'days': 'Sunday',
+            'times': '11am - 6pm'
+        }
+    ];
 
     const render = () => {
         const contentDiv = document.querySelector('#content');
         contentDiv.innerHTML = '';
 
-        const homeElement =  document.createElement('div');
-        homeElement.setAttribute('id', 'home');
-        homeElement.textContent = 'THIS IS THE HOME PAGE!';
+        const homeDiv =  document.createElement('div');
+        homeDiv.setAttribute('id', 'home-div');
+        homeDiv.classList.add('page-content');
 
-        contentDiv.appendChild(homeElement);
+        const h1 = document.createElement('h1');
+        h1.textContent = `Welcome to Jimmy's Restaurant`
+        homeDiv.appendChild(h1);
+
+        const about = document.createElement('div');
+        about.textContent = `Jimmy's is a swell place for nice people. We have food here that you can put in your mouth. It is good.`;
+        homeDiv.appendChild(about);
+
+        const openingTimes = document.createElement('div');
+        for (const day in openingTimesArr) {
+            const div = document.createElement('div');
+            div.textContent = `${openingTimesArr[day].days}: ${openingTimesArr[day].times}`
+            openingTimes.appendChild(div);
+        }
+        homeDiv.appendChild(openingTimes);
+
+        const location = document.createElement('div');
+        location.textContent = `123 place, Nicetown, County, Postcode`;
+        homeDiv.appendChild(location);
+
+        contentDiv.appendChild(homeDiv);
     }
     
     return {
